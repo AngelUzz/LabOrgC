b6e4953b8a0dc046a47d3559081d96b08c9b45df
AngelUzz <angel.uribe50@uabc.edu.mx>
2025-10-23 18:58:25 +0000

    si

---
+++ P5/p5e.asm
@@ -0,0 +1,19 @@
+%include "../LIB/pc_io.inc"
+
+section	.text
+	global _start       ;referencia para inicio de programa
+	
+_start:                   
+	mov edx, msg		; edx = dirección de la cadena msg
+	call puts			; imprime cadena msg terminada en valor nulo (0)
+
+	mov ebx, edx
+    mov esi, 20
+    mov byte [ebx+esi+5], 'Z'
+    call puts
+    
+	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
+	int	0x80        	; llamada al sistema - fin de programa
+
+section	.data
+msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
---
+++ P6/p6.asm
@@ -0,0 +1,10 @@
+%include "../LIB/pc_iox.inc"
+
+section	.text
+
+	global _start       ;must be declared for using gcc
+
+_start:
+
+    mov ebx, 0x5C4B2A60
+    
